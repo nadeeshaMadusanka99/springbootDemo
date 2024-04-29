59d6e8d9bb75e7aa184f2d95c5f6bc9b0a557cd7
Nadeesha Madusanka <110985815+nadeeshaMadusanka99@users.noreply.github.com>
2024-04-29 10:36:47 +0530

    Add Post method to save a user in the db

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -1,11 +1,16 @@
 package com.example.demo.controller;
 
+import com.example.demo.dto.UserDTO;
+import com.example.demo.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value = "api/v1/user")
 @CrossOrigin
 public class UserController {
+    @Autowired
+    private UserService userService;
 
     @GetMapping("/getUser")
     public String getUser() {
@@ -13,8 +18,8 @@ public class UserController {
     }
 
     @PostMapping("/saveUser")
-    public String saveUser(){
-        return "user saved!";
+    public UserDTO saveUser(@RequestBody UserDTO userDTO){
+        return userService.saveUser(userDTO);
     }
 
     @PutMapping("/updateUser")
