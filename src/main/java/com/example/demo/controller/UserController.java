8c5d8c06c7e8b79a124e3ce7c9ff6f60d195bc3f
Nadeesha Madusanka <110985815+nadeeshaMadusanka99@users.noreply.github.com>
2024-04-29 11:17:15 +0530

    Add get method to get all users in the db

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -5,6 +5,8 @@ import com.example.demo.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping(value = "api/v1/user")
 @CrossOrigin
@@ -12,9 +14,9 @@ public class UserController {
     @Autowired
     private UserService userService;
 
-    @GetMapping("/getUser")
-    public String getUser() {
-        return "user-Nadeesha";
+    @GetMapping("/getUsers")
+    public List<UserDTO> getUsers() {
+        return userService.getAllUsers();
     }
 
     @PostMapping("/saveUser")
--- src/main/java/com/example/demo/service/UserService.java
+++ src/main/java/com/example/demo/service/UserService.java
@@ -4,9 +4,12 @@ import com.example.demo.dto.UserDTO;
 import com.example.demo.entity.User;
 import com.example.demo.repo.UserRepo;
 import org.modelmapper.ModelMapper;
+import org.modelmapper.TypeToken;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 @Transactional
@@ -19,5 +22,10 @@ public class UserService  {
     public UserDTO saveUser(UserDTO userDTO){
         userRepo.save(modelMapper.map(userDTO, User.class));
         return userDTO;
+    }
+
+    public List<UserDTO> getAllUsers(){
+        List<User>userList = userRepo.findAll();
+        return modelMapper.map(userList, new TypeToken<List<UserDTO>>(){}.getType());
     }
 }
